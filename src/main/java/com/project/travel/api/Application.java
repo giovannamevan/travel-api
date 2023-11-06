a1b129a656510e7bb9df0beb483cdf4ccd6ab4de
Giovanna <giovannaevangelista78@gmail.com>
2023-11-06 17:45:43 -0300

    novo banco de dados, path html e configuração de pom.xml

--- src/main/java/com/project/travel/api/Application.java
+++ src/main/java/com/project/travel/api/Application.java
@@ -17,7 +17,7 @@ public class Application {
 	}
 
 	private static void abrirPaginaHTML() {
-		String htmlFilePath = "C:/Users/User/Desktop/Facul/POO/travel-api/src/main/resources/Frontend/home.html";
+		String htmlFilePath = "C:\\Users\\giova\\OneDrive\\Área de Trabalho\\Faculdade\\POO2\\travel-api\\src\\main\\resources\\Frontend\\home.html";
 		String browserCommand = "cmd /c start \"\" \"" + htmlFilePath + "\"";
 
 		try {
